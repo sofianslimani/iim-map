03e984d68734bd381f3d09d8cb6f5cdcdbf99165
Thomas Cardiet <thomas@MacBook-Air-de-Thomas.local>
2022-12-06 13:48:44 +0100

    add restaurants auto

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const lat = 48.852969;
-const lon = 2.349903;
+const lat = 48.89347;
+const lon = 2.232;
 let map = null;
 
 const restaurants = [
@@ -8,18 +8,21 @@ const restaurants = [
     lng: 2.229,
     marker: null,
     name: 'KFC',
+    img: 'https://media-cdn.tripadvisor.com/media/photo-s/05/cf/59/60/kfc-jaux.jpg',
   },
   {
     lat: 48.8979,
     lng: 2.2252,
     marker: null,
     name: 'MCDonald',
+    img: 'https://media.mcdonalds.fr/media/d5/28/8d/d5288d1b71c580c24bd5da511c2656251640d1ec?auto=webp',
   },
   {
     lat: 48.89433,
     lng: 2.2243,
     marker: null,
     name: 'BurgerKing',
+    img: 'https://static.observatoiredelafranchise.fr/images/ckfinder/images/2020/07/17/burger-king-ext-live-7bc2e3.png',
   },
 ];
 
@@ -81,6 +84,32 @@ const arrival = {
 
 const miam = 13;
 
+const restaurantListHtml = document.querySelector('.restaurants__list');
+function PrintRestaurants() {
+  restaurantListHtml.innerHTML = '';
+  restaurants.forEach((restaurant) => {
+    restaurantListHtml.innerHTML += `<li class="restaurants__list__item">
+      <div class="restaurants__list__item__image">
+        <img
+          src="${restaurant.img}"
+          alt=""
+        />
+      </div>
+
+      <div class="restaurants__list__item__content">
+        <div class="restaurants__list__item__name">
+          <h3>${restaurant.name}</h3>
+        </div>
+        <div class="restaurants__list__item__address">Address 1</div>
+        <div class="restaurants__list__item__phone">Phone 1</div>
+        <div class="restaurants__list__item__website">Website 1</div>
+      </div>
+    </li>`;
+  });
+}
+
+PrintRestaurants();
+
 // GET DISTANCE
 function getDistance(A, B) {
   const R = 6371e3;
@@ -101,7 +130,7 @@ function getTrajectTime(person) {}
 
 // INIT MAP
 function initMap() {
-  map = L.map('map').setView([lat, lon], 11);
+  map = L.map('map').setView([lat, lon], 15);
 
   L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
     minZoom: 1,
@@ -131,7 +160,7 @@ function initMap() {
 
   // ON DRAG ARRIVAL MARKER
   arrival.marker.on('drag', function () {
-    console.log(getDistance(persons.list[0].position, arrival.marker._latlng));
+    // console.log(getDistance(persons.list[0].position, arrival.marker._latlng));
   });
 }
 
