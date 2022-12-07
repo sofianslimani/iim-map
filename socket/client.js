d3a542cc3bca50b5dd1d63b467c855a26a75ed2c
Thomas Cardiet <thomas@MacBook-Air-de-Thomas.local>
2022-12-07 11:12:46 +0100

    addUser Logic

--- socket/client.js
+++ socket/client.js
@@ -4,17 +4,25 @@
   $('#loginform').submit(function (event) {
     event.preventDefault();
 
+    const name = $('#name').val();
+    const coords = $('#login__coords').val();
+
+    if (!name || !coords)
+      return console.log('Merci de remplir tous les champs');
+
     addUser({
-      name: $('#name').val(),
+      name: name,
       restaurant: parseInt($('#login__restaurant').val()),
       position: {
-        lat: parseFloat($('#login__coords').val().split(';')[0]),
-        lng: parseFloat($('#login__coords').val().split(';')[1]),
+        lat: parseFloat(coords.split(';')[0]),
+        lng: parseFloat(coords.split(';')[1]),
       },
     });
 
+    toggleLoginPopup(false);
+
     /*socket.emit('login', persons.list);*/
 
-    console.log('login', persons.list);
+    console.log('login', users.list);
   });
 })(jQuery);
